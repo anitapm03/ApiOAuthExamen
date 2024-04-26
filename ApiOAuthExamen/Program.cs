5767a574ad5088c102aee560ba230c0b06e4fd64
anitapm03 <apereiramolinero@gmail.com>
2024-04-26 13:28:13 +0200

    va con key vault basico

--- ApiOAuthExamen/Program.cs
+++ ApiOAuthExamen/Program.cs
@@ -2,12 +2,45 @@ using ApiOAuthExamen.Data;
 using ApiOAuthExamen.Helpers;
 using ApiOAuthExamen.Repositories;
 using ApiOAuthExamen.Services;
+using Azure.Security.KeyVault.Secrets;
 using Azure.Storage.Blobs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+/*
+ para cuando haya keys
+    builder.Services.AddAzureClients(factory =>
+    {
+    factory.AddSecretClient
+        (builder.Configuration.GetSection("KeyVault");
+    });
+
+SecretClient secretClient = 
+    builder.Services.BuildServiceProvider().GetService<SecretClient>();
+KeyVaultSecret secretSql = await 
+    secretClient.GetSecretAsync("SqlAzure");
+KeyVaultSecret secretIssuer = await 
+    secretClient.GetSecretAsync("Issuer");
+KeyVaultSecret secretAudience = await 
+    secretClient.GetSecretAsync("Audience");
+KeyVaultSecret secretKey = await 
+    secretClient.GetSecretAsync("SecretKey");
+ */
+builder.Services.AddAzureClients(factory =>
+{
+    factory.AddSecretClient
+        (builder.Configuration.GetSection("KeyVault"));
+});
+SecretClient secretClient =
+    builder.Services.BuildServiceProvider().GetService<SecretClient>();
+
+KeyVaultSecret secretSql = await
+    secretClient.GetSecretAsync("SQL");
+
+
 string azureKeys = builder.Configuration.GetValue<string>
     ("AzureKeys:StorageAccount");
 BlobServiceClient blobServiceClient =
@@ -24,8 +57,8 @@ builder.Services.AddAuthentication
     (helper.GetAuthenticationSchema())
     .AddJwtBearer(helper.GetJwtBearerOptions());
 
-string connectionString =
-    builder.Configuration.GetConnectionString("SQL");
+string connectionString = secretSql.Value;
+    //builder.Configuration.GetConnectionString("SQL");
 builder.Services.AddTransient<RepositoryCubos>();
 builder.Services.AddDbContext<CubosContext>
     (options => options.UseSqlServer(connectionString));
